c917624562e30b071e35910fd7cfd03df299bf0c
metazilla <evilgeniuscow@gmail.com>
2018-05-02 05:00:12 -0700

    fumen add-on to make my output.txt into a nice diagram

---
+++ fumen/english.js
@@ -0,0 +1,45 @@
+// Add-on
+// English translation
+// Rev.4
+
+STRING_FIELD_SETTING = "Place Blocks"; // "フィールド作成";
+STRING_FIELD_SHIFT_UP = "↑ Up"; // "↑ 上シフト";
+STRING_FIELD_SHIFT_LEFT = "← Left"; // "← 左シフト";
+STRING_FIELD_SHIFT_RIGHT = "→ Right"; // "→ 右シフト";
+STRING_FIELD_SHIFT_DOWN = "↓ Down"; // "↓ 下シフト";
+STRING_FIELD_FILL_ROW = "Fill Row"; // "行";
+STRING_MINO_SETTING = "Mino"; // "ミノ配置";
+STRING_MINO_GUIDELINE = "Guideline"; // "色";
+STRING_MINO_QUIZ = "Quiz"; // "問";
+STRING_EFFECT_SETTING = "After Placement"; // "直後に:";
+STRING_EFFECT_LOCKDOWN = "Lock"; // "ミノ接着";
+STRING_EFFECT_GARBAGE = "Rise"; // "せり上がり";
+STRING_EFFECT_MIRROR = "Mirror"; // "ミラー";
+STRING_FRAME_NUMBER = "Frame"; // "ページ:";
+STRING_FRAME_CAPTION = "≪ Caption"; // "≪";
+STRING_FRAME_FIRST = "|&lt;";
+STRING_FRAME_PREVIOUS = "▲ Back"; // "▲ 前";
+STRING_FRAME_NEXT = "▼ Next"; // "▼ 次";
+STRING_FRAME_LAST = "&gt;|";
+STRING_FRAME_TRUNCATE = "× Clear to end"; // "× 次ページ以降を削除";
+STRING_TRUNCATE_CONFIRM =
+  "Are you sure you want to clear all following frames?"; // "次ページ以降を完全に削除してよろしいですか？";
+STRING_CODE_OUTPUT = "Output Data"; // "★ データ出力";
+STRING_CODE_AUTOOUTPUT = "Auto Output"; // "自動";
+STRING_CODE_NEW = "＊ New Data"; // "＊ 新規作成";
+STRING_CODE_LOAD = "☆ Load Data"; // "☆ データ読み込み";
+STRING_DESTROY_CONFIRM = "※ Current data will be lost."; // "※ 作成中のデータは失われます。";
+
+STRING_FRAME_DELETE = "－ Delete current frame"; // "－ 現ページ削除";
+STRING_FRAME_INSERT = "＋ Copy current frame"; // "＋ 現ページ複製";
+STRING_FRAME_DELPAST = "／ Clear past frames"; // "／ 前ページまでを削除";
+STRING_FRAME_IMPORT = "＃ Append Data"; // "＃ 追加読み込み";
+STRING_DELPAST_CONFIRM = "Are you sure you want to clear all past frames?"; // "前ページまでを完全に削除してよろしいですか？";
+
+STRING_WIKICODE_UPPERCASE = "Upper-case"; // "大文字";
+STRING_WIKICODE_OUTPUT = "Output Diagram code"; // "Wikiコード出力";
+
+STRING_LISTING_SHOW_CAPTION = "List of captions"; // "コメントリスト";
+STRING_LISTING_SHOW_ALLFRAMES = "List of frames"; // "全ページリスト";
+STRING_LISTING_AUTOJUMP = "Auto Jump"; // "自動的に移動";
+STRING_LISTING_JUMP = "Go"; // "移動";
